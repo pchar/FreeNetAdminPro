8854d3a299bc19f4a36e328bfea79973510af8b4
charbo <chapat@gmail.com>
2026-05-24 23:09:52 +0200

    ok resources

---
+++ rc_resources.py
@@ -0,0 +1,74 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.10.3
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x00\xca\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 viewBox=\x22\
+0 0 24 24\x22 fill=\
+\x22none\x22 stroke=\x22#\
+22c55e\x22 stroke-w\
+idth=\x222\x22 stroke-\
+linecap=\x22round\x22 \
+stroke-linejoin=\
+\x22round\x22>\x0a  <poly\
+gon points=\x225 3 \
+19 12 5 21 5 3\x22/\
+>\x0a</svg>\x0a\
+\x00\x00\x00\xd5\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 viewBox=\x22\
+0 0 24 24\x22 fill=\
+\x22none\x22 stroke=\x22#\
+ef4444\x22 stroke-w\
+idth=\x222\x22 stroke-\
+linecap=\x22round\x22 \
+stroke-linejoin=\
+\x22round\x22>\x0a  <rect\
+ x=\x224\x22 y=\x224\x22 wid\
+th=\x2216\x22 height=\x22\
+16\x22 rx=\x222\x22/>\x0a</s\
+vg>\x0a\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x09\
+\x08\x97\xaf\x87\
+\x00s\
+\x00t\x00a\x00r\x00t\x00.\x00s\x00v\x00g\
+\x00\x08\
+\x0bcU\x87\
+\x00s\
+\x00t\x00o\x00p\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x9e[\xb0w\xc9\
+\x00\x00\x00(\x00\x00\x00\x00\x00\x01\x00\x00\x00\xce\
+\x00\x00\x01\x9e[\xb0w\xd2\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
